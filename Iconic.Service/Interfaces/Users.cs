79352b0f7c412ca59be0b8cb7e31d8d70dfc1875
Temurkhann <temurxonshodiyev4@gmail.com>
2023-10-15 04:29:12 +0500

    Changes created

---
+++ Iconic.Service/Interfaces/Users.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iconic.Service.Interfaces
+{
+    internal class Users
+    {
+    }
+}
